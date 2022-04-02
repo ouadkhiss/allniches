d466a72d64dc91a2e3a8cada649beff6d7a1e959
ouadkhiss <46076495+ouadkhiss@users.noreply.github.com>
2022-04-02 16:12:51 +0200

    upload

---
+++ config/config.php
@@ -0,0 +1,19 @@
+<?php  
+
+$seo_url = 'robux-gen-free.orgfree.com';  
+$seo_title = 'Free Robux Generator';  
+$seo_description = 'Roblox Free Robux Generator';  
+$seo_keywords = 'Free Robux, Get Free Robux, Earn Free Robux';  
+$seokey = 'Free Robux, Get Free Robux, Earn Free Robux';  
+
+$title = 'Robux FREE Geneartor';
+$appgame = 'ROBLOX'; 
+$rewards = 'ROBUX';  
+
+$colorone = '#4b974b';  
+$colortwo = '#66c466';  
+
+$cpa_it = '1750972';	  
+$cpa_key = '5dc52';	  
+
+?>
